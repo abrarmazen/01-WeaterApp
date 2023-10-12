e5de7e466fee9a1dcacf089b423998a2eb7d4fc7
Abrar Mohammed <48630489+abrarmazen@users.noreply.github.com>
2023-10-12 03:10:25 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # Weather-Journal App Project
 
-## Overview
-This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
+## description
+This is the Weather project in profisional web trak from udacity.
+In This project we need alot of feature to build it.
+We need the node.js in your presonal desktop and install express.
+We have to file server.js and app.js.
+This project use to featch data from api in open weather .
+when user input zip code and feelling and click on button show the  zip code and date and felling to user.
+we use get and post requist and use async function.
 
-## Instructions
-This will require modifying the `server.js` file and the `website/app.js` file. You can see `index.html` for element references, and once you are finished with the project steps, you can use `style.css` to style your application to customized perfection.
+# Thanks
+Thank you to all of the sponsors of this scholarship, and thanks to all the lecturers.
 
-## Extras
-If you are interested in testing your code as you go, you can use `tests.js` as a template for writing and running some basic tests for your code.
+## Technolgy
+1.node.js
+2.express
+3.html
+4.javaScript
+5.css
+## Aouther
+Abrar Mohammed Hassan
+ ## resoucses
+ 1.https://expressjs.com/en/guide/routing.html
+ 2.https://classroom.udacity.com/nanodegrees/nd0011-fwd-t2/parts/cd0429
+ 3.https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs
+ 4.https://udacity.zoom.us/rec/play/lNVEzXto5PvpIwCZh1ZwmrinS7vkLhjZU1_GndH9_cCDYCRy84LoFaoOSYEQhk5fW-wX0EUm3fK87vaw.uhVtgxiD1IGeR8ui?continueMode=true&_x_zm_rtaid=9l-Jb-jhQDafgG-8et1QaA.1644325854106.2a8c27604fbbdc89d98dbb6ef291dc3d&_x_zm_rhtaid=29
+
+Thanks Udacity
--- server.js
+++ server.js
@@ -1,41 +1,49 @@
-// Setup empty JS object to act as endpoint for all routes
+// javaScript empty object 
 projectData = {};
+//my own port
 const port = 4000
 // Require Express to run server and routes
 const express = require('express');
+//cors this allow server to indicate any origins
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+//app is instance from express
 const app = express();
 
-// Start up an instance of app
-
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+//use body parsing middleware.
+//body-barser is debrecated 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 app.use(cors());
 
-// Initialize the main project folder
+// instance app pointed to the folder website
 app.use(express.static('website'));
-
+//using function listen to run localhost 
 app.listen( port , () => {
-    console.log(`your port code is ${port}`)
+    console.log("you add a correct port")
+    console.log(`the port to this project is :${port}`)
 })
-const funGet = (req , res) => {
+//using call back function to get 
+const callGet = (req , res) => {
+    //return javaScript object 
+    console.log("your get data to weather")
     res.send(projectData)
     //projectData = {}
 }
-const funpost = (req,res) => {
+
+//call back function to post 
+const callpost = (req , res) => {
     console.log(req.body)
     projectData.temp = req["body"]["temp"],
     projectData.date = req["body"]["date"],
     projectData.felling = req["body"]["felling"],
    
-    //projectData={...req.body}
+    console.log("your post data to weather")
     res.end()
 }
-app.get("/anynameget" , funGet)
-app.post("/postAnyName",funpost)
+//to get endboint 
+app.get("/anynameget" , callGet)
+//post requeste
+app.post("/postAnyName", callpost)
